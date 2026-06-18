ef3f76fee4d41f41be3f413ea8d1dc70457a5638
amalaajkamal <amalajkamal@gmail.com>
2026-06-17 20:35:35 -0400

    Update gbd_afu_dashboard.py

--- gbd_afu_dashboard.py
+++ gbd_afu_dashboard.py
@@ -29,7 +29,7 @@ st.set_page_config(
 st.markdown("""
 <style>
     .main-title {
-        font-size: 1.9rem;
+        font-size: 2.5rem;
         font-weight: 600;
         color: #4D9FE8;
         margin-bottom: 0.2rem;
